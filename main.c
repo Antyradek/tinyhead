f6514690d042f6064316e605bb83f64dd4eb48a4
Antyradek <antyradek@gmail.com>
2017-07-24 00:47:11 +0200

    Fix indentation (stupid KWrite)

--- main.c
+++ main.c
@@ -16,18 +16,6 @@
 
 typedef unsigned char byte;
 
-void printUsage(const char* appName, const char* varNameData, const char* varNameSize)
-{
-	printf("Usage: %s [OPTIONS]\n\n", appName);
-	printf("Encodes given data as array of bytes in C/C++ header file, which can be included into source code and will be put into data memory fragment on execution. Header includes two variables with data pointer and size.\n\n");
-	
-	printf("Program reads FILENAME file and prints header to STDOUT. \nIf no FILENAME is given, reads from STDIN until EOF (^D). File can be empty.\n");
-	printf("\t-v VARNAME\tDefault variable names are const unsigned char %s%s[] and const size_t %s%s. \n\t\t\tUse this option to change them to VARNAME%s[] and VARNAME%s.\n", DEFAULT_VARNAME, varNameData, DEFAULT_VARNAME, varNameSize, varNameData, varNameSize);
-	printf("\t-s\t\tUse snake case (my_amazing_file_data) instead of camel case (myAmazingFileData).\n");
-	printf("\t-f FILENAME\tFile to read contents from, if not given, reads from STDIN.\n");
-	printf("\t-h, --help\tPrint this help screen.\n");
-}
-
 int main(int argc, char** argv)
 {
 	//execution context
@@ -35,7 +23,7 @@ int main(int argc, char** argv)
 	char* varName = DEFAULT_VARNAME;
 	const char* appName = argv[0];
 	char* varNameData = "Data";
-    char* varNameSize = "DataSize";
+	char* varNameSize = "DataSize";
 	int useSnakeCase = 0;
 	int printVersion = 0;
 	
@@ -85,68 +73,68 @@ int main(int argc, char** argv)
 		varNameSize = "_data_size";
 	}
 	
-    int retVal = 0;
-
-    if(filename != NULL)
-    {
-        //Those are C exceptions with goto
-        //open file
-        FILE* file = fopen(filename, "rb");
-        if(file == NULL)
-        {
-            perror(appName);
-            retVal = EXIT_ERR_READ;
-            goto fopenErr;
-        }
-
-        //this function may return non-zero with empty files and ftell will still work correctly
-        fseek(file, 0, SEEK_END);
-
-        //get file size
-        const long fileSize = ftell(file);
-        if(fileSize < 0)
-        {
-            perror(appName);
-            retVal = EXIT_ERR_READ;
-            goto ftellErr;
-        }
-
-        rewind(file);
-
-        //allocate buffer
-        byte* buffer = malloc(sizeof(byte) * fileSize);
-        if(buffer == NULL)
-        {
-            perror(appName);
-            retVal = EXIT_ERR_MEM;
-            goto mallocErr;
-        }
-
-        //read file into memory
-        const size_t readBytes = fread(buffer, sizeof(byte), fileSize, file);
-        if(ferror(file))
-        {
-            perror(appName);
-            retVal = EXIT_ERR_READ;
-            goto freadErr;
-        }
-
-        //print header
-        puts("#pragma once");
-        printf("const size_t %s%s = %lu;\n", varName, varNameSize, fileSize);
-        printf("const unsigned char %s%s[] = {", varName, varNameData);
-
-        //create buffer file, to output everything at once
+	int retVal = 0;
+	
+	if(filename != NULL)
+	{
+		//Those are C exceptions with goto
+		//open file
+		FILE* file = fopen(filename, "rb");
+		if(file == NULL)
+		{
+			perror(appName);
+			retVal = EXIT_ERR_READ;
+			goto fopenErr;
+		}
+		
+		//this function may return non-zero with empty files and ftell will still work correctly
+		fseek(file, 0, SEEK_END);
+		
+		//get file size
+		const long fileSize = ftell(file);
+		if(fileSize < 0)
+		{
+			perror(appName);
+			retVal = EXIT_ERR_READ;
+			goto ftellErr;
+		}
+		
+		rewind(file);
+		
+		//allocate buffer
+		byte* buffer = malloc(sizeof(byte) * fileSize);
+		if(buffer == NULL)
+		{
+			perror(appName);
+			retVal = EXIT_ERR_MEM;
+			goto mallocErr;
+		}
+		
+		//read file into memory
+		const size_t readBytes = fread(buffer, sizeof(byte), fileSize, file);
+		if(ferror(file))
+		{
+			perror(appName);
+			retVal = EXIT_ERR_READ;
+			goto freadErr;
+		}
+		
+		//print header
+		puts("#pragma once");
+		printf("const size_t %s%s = %lu;\n", varName, varNameSize, fileSize);
+		printf("const unsigned char %s%s[] = {", varName, varNameData);
+		
+		//create buffer file, to output everything at once
 		FILE* bufferFile = tmpfile();
 		if(bufferFile == NULL)
-        {
-            perror(appName);
-            retVal = EXIT_ERR_MEM;
-            goto bufferFileErr;
-        }
-        
-        //read buffer, print to buffer file
-        size_t i = 0;
+		{
+			perror(appName);
+			retVal = EXIT_ERR_MEM;
+			goto bufferFileErr;
+		}
+		
+		//read buffer, print to buffer file
+		size_t i = 0;
 		if(readBytes > 0)
 		{
 			for(i = 0; i < readBytes - 1; i++)
@@ -158,60 +146,60 @@ int main(int argc, char** argv)
 				fprintf(bufferFile,"%hhu", buffer[i]);
 			}
 		}
-        fprintf(bufferFile, "};\n");
-
-        //print buffer file at once
-        const long tmpFileSize = ftell(bufferFile);
-        rewind(bufferFile);
-
-        char* bufferBuffer = malloc(sizeof(char) * tmpFileSize);
-        if(bufferBuffer == NULL)
-        {
-            perror(appName);
-            retVal = EXIT_ERR_MEM;
-            goto bufferBufferErr;
-        }
-
-        fread(bufferBuffer, sizeof(char), tmpFileSize, bufferFile);
-        if(ferror(bufferFile))
-        {
-            perror(appName);
-            retVal = EXIT_ERR_MEM;
-            goto bufferBufferFileErr;
-        }
-        fputs(bufferBuffer, stdout);
-
-    bufferBufferFileErr:
-    bufferBufferErr:
-        free(bufferBuffer);
-    bufferFileErr:
-        fclose(bufferFile);
-    freadErr:
-        free(buffer);
-    mallocErr:
-    ftellErr:
-        fclose(file);
-    fopenErr:
+		fprintf(bufferFile, "};\n");
+		
+		//print buffer file at once
+		const long tmpFileSize = ftell(bufferFile);
+		rewind(bufferFile);
+		
+		char* bufferBuffer = malloc(sizeof(char) * tmpFileSize);
+		if(bufferBuffer == NULL)
+		{
+			perror(appName);
+			retVal = EXIT_ERR_MEM;
+			goto bufferBufferErr;
+		}
+		
+		fread(bufferBuffer, sizeof(char), tmpFileSize, bufferFile);
+		if(ferror(bufferFile))
+		{
+			perror(appName);
+			retVal = EXIT_ERR_MEM;
+			goto bufferBufferFileErr;
+		}
+		fputs(bufferBuffer, stdout);
+		
+	bufferBufferFileErr:
+	bufferBufferErr:
+		free(bufferBuffer);
+	bufferFileErr:
+		fclose(bufferFile);
+	freadErr:
+		free(buffer);
+	mallocErr:
+	ftellErr:
+		fclose(file);
+	fopenErr:
 		poptFreeContext(optCon);
-        return retVal;
-    }
-    else
-    {
-        puts("#pragma once");
-        printf("const unsigned char %s%s[] = {", varName, varNameData);
-
-        byte* buffer = malloc(sizeof(byte) * BUFFER_SIZE);
+		return retVal;
+	}
+	else
+	{
+		puts("#pragma once");
+		printf("const unsigned char %s%s[] = {", varName, varNameData);
+		
+		byte* buffer = malloc(sizeof(byte) * BUFFER_SIZE);
 		if(buffer == NULL)
 		{
 			perror(appName);
 			retVal = EXIT_ERR_MEM;
 			goto pipeMallocErr;
 		}
-
-        size_t fileSize = 0;
-        while(true)
-        {
-            int readBytes = fread(buffer, sizeof(char), BUFFER_SIZE, stdin);
+		
+		size_t fileSize = 0;
+		while(true)
+		{
+			int readBytes = fread(buffer, sizeof(char), BUFFER_SIZE, stdin);
 			if(ferror(stdin))
 			{
 				perror(appName);
@@ -220,37 +208,37 @@ int main(int argc, char** argv)
 			}
 			fileSize += readBytes;
 			int i = 0;
-
+			
 			if(readBytes == BUFFER_SIZE)
-            {
-                for(i = 0; i < readBytes; i++)
-                {
-                    printf("%hhu,", buffer[i]);
-                }
-            }
-            else if(readBytes != BUFFER_SIZE && feof(stdin))
-            {
-                for(i = 0; i < readBytes - 1; i++)
-                {
-                    printf("%hhu,", buffer[i]);
-                }
-                if(i < readBytes)
-                {
-                    printf("%hhu", buffer[i]);
-                }
-                printf("};\n");
-                break;
-            }
-        }
-        printf("const size_t %s%s = %lu;\n", varName, varNameSize, fileSize);
-
+			{
+				for(i = 0; i < readBytes; i++)
+				{
+					printf("%hhu,", buffer[i]);
+				}
+			}
+			else if(readBytes != BUFFER_SIZE && feof(stdin))
+			{
+				for(i = 0; i < readBytes - 1; i++)
+				{
+					printf("%hhu,", buffer[i]);
+				}
+				if(i < readBytes)
+				{
+					printf("%hhu", buffer[i]);
+				}
+				printf("};\n");
+				break;
+			}
+		}
+		printf("const size_t %s%s = %lu;\n", varName, varNameSize, fileSize);
+		
 	pipeFreadError:
-        free(buffer);
+		free(buffer);
 	pipeMallocErr:
 		poptFreeContext(optCon);
 		return retVal;
-    }
-    //execution should never reach here
-    poptFreeContext(optCon);
+	}
+	//execution should never reach here
+	poptFreeContext(optCon);
 	return 0;
 }
